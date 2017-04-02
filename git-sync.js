c74d3208834324c944c8dd892ac87f6e8b670b10
Vikram Tiwari <vikramtheone1@gmail.com>
2017-04-01 20:22:02 -0700

    update git-sync
    - remove local directory before cloning
    - ability to pass a branch to clone
    - postinstall should take care of cloning repo

--- git-sync.js
+++ git-sync.js
@@ -2,13 +2,16 @@
 
 const git = require('nodegit')
 const path = require('path')
+const fs = require('fs-extra')
 
 const debug = require('debug')('static-node-server:git-sync')
 debug(`Starting sync..`)
 
 const remoteUrl = 'https://github.com/VikramTiwari/static-html-git-sync.git'
 const localPath = path.join(__dirname, 'public')
-const cloneOptions = {}
+const cloneOptions = {
+  checkoutBranch: 'master'
+}
 
 /**
  * Clone remote to local path with options
@@ -17,7 +20,10 @@ const cloneOptions = {}
  * @param {string} localPath
  * @param {object} cloneOptions
  */
-function clone (remoteUrl, localPath, cloneOptions) {
+function clone(remoteUrl, localPath, cloneOptions) {
+  // first cleanup the local directory
+  fs.removeSync(localPath)
+  // then clone remote into local
   git.Clone(remoteUrl, localPath, cloneOptions).then(function (repo) {
     debug(`Cloned ${path.basename(remoteUrl)} to ${repo.workdir()}`)
   }).catch(function (error) {
